19b29b5eb10d46d8fe4349ff439e67902128bfaa
ADernild <alex@dernild.dk>
2021-02-21 15:56:24 +0100

    script til at lave kort over stoppesteder og html fil med kort

---
+++ scripts/stoppesteder_kort.R
@@ -0,0 +1,16 @@
+library(tidyverse)
+library(leaflet)
+
+df <- read.csv("Fynske_stoppesteder_rettet.csv")
+
+m <- leaflet(df) %>% 
+  addTiles() %>% 
+  setView(lng = 10.3, lat = 55.2, zoom = 9) %>% 
+  addCircles(
+    lng = ~S_LONG,
+    lat = ~S_LAT,
+    label = ~Stopnavn,
+    color = "coral",
+    radius = 5,
+    weight = 2)
+m
