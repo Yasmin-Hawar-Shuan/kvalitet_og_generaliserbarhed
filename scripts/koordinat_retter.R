bf88432459bfbe23ca4f22c2ce93cd9803be6e8f
ADernild <alex@dernild.dk>
2021-02-25 12:44:58 +0100

    lille rettelser i hvilke koordinater der droppes

--- scripts/koordinat_retter.R
+++ scripts/koordinat_retter.R
@@ -12,9 +12,11 @@ df$S_LAT <- df$S_LAT %>%
   str_replace("^55", "55.") %>% # bytter 55, hvis første karakterer, ud med 55.
   str_replace("^54", "54.") # bytter 54, hvis første karaterer, ud med 54.
 
-df$S_LONG[ df$S_LONG == "0" | df$S_LONG == "NULL" | df$S_LONG == "11"] <- NA # laver 0, 11 og NULL til NA
-df$S_LAT[ df$S_LAT == "0" | df$S_LAT == "NULL" | df$S_LAT == "734" | df$S_LAT == "55"] <- NA # laver 0, NULL, 55 og 734 til NA
+df$S_LONG[ df$S_LONG == "0" | df$S_LONG == "NULL" | df$S_LONG == "10." | df$S_LONG == "11"] <- NA # laver 0, 11 og NULL til NA
+df$S_LAT[ df$S_LAT == "0" | df$S_LAT == "NULL" | df$S_LAT == "734" | df$S_LAT == "55."] <- NA # laver 0, NULL, 55 og 734 til NA
 missing_coord <- df$Stopnavn[is.na(df$S_LAT) & df$Stopnavn != "NULL"] # stoppesteder med manglende koordinater
 
 
 write.csv(df, "Fynske_stoppesteder_rettet.csv")
+
+
